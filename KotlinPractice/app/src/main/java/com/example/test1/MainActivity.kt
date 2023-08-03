76a2aed2bd4992629cf983a588a0db4184282519
kivccc <71814049+kivccc@users.noreply.github.com>
2023-08-03 17:05:48 +0900

    Main,Sub Activity

--- KotlinPractice/app/src/main/java/com/example/test1/MainActivity.kt
+++ KotlinPractice/app/src/main/java/com/example/test1/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.test1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.test1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val activityResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            if(it.resultCode== RESULT_OK) {
+                val message = it.data?.getStringExtra("returnValue")
+                Toast.makeText(this,message,Toast.LENGTH_LONG).show()
+            }
+        }
+
+        val intent = Intent(this,SubActivity::class.java)
+        intent.putExtra("from1","Hello Bundle")
+        intent.putExtra("from2",2021)
+        binding.btnStart.setOnClickListener { activityResult.launch(intent) }
+
+
     }
 }
